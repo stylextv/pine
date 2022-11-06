a5877a10c7d327704c188d6ff30c21fef52aa439
StylexTV <stylextv42@gmail.com>
2022-11-06 19:45:20 +0100

    Update CastleMoveConstraint.java

--- src/net/pine/game/move/constraint/constraints/CastleMoveConstraint.java
+++ src/net/pine/game/move/constraint/constraints/CastleMoveConstraint.java
@@ -1,12 +1,17 @@
 package net.pine.game.move.constraint.constraints;
 
 import net.pine.game.board.Board;
+import net.pine.game.move.MoveFlag;
 import net.pine.game.move.constraint.MoveConstraint;
 
 public class CastleMoveConstraint extends MoveConstraint {
 	
 	@Override
 	public boolean isIllegalMove(int move, Board board) {
+		int flag = MoveFlag.ofMove(move);
+		
+		if(flag != MoveFlag.CASTLE) return false;
+		
 		// don't castle in or through check
 		return false;
 	}
