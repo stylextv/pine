13f56e32b8edcf24c6dbd9d8712f8cc49f3ff727
StylexTV <stylextv42@gmail.com>
2022-11-07 09:44:05 +0100

    Update CastleProperty.java

--- src/net/pine/game/board/property/properties/CastleProperty.java
+++ src/net/pine/game/board/property/properties/CastleProperty.java
@@ -6,7 +6,8 @@ public class CastleProperty extends StackProperty {
 	
 	@Override
 	public void onMove(int move) {
-		
+		// on king move -> lose all castle perms
+		// on rook move or capture -> lose castle perm for that side
 	}
 	
 	public boolean canCastle(int color, int side) {
